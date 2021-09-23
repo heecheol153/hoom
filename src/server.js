3842235191bdbf450e0881cb05517f60d0c56804
heecheol153 <heecheol153@gmail.com>
2021-09-23 22:13:59 +0900

    #2.3 Recap

--- src/server.js
+++ src/server.js
@@ -14,20 +14,20 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  socket.on("enter_room", (msg, done) => {
-    console.log(msg);
+  socket.on("enter_room", (roomName, done) => {
+    console.log(roomName);
     setTimeout(() => {
-      done();
-    }, 10000);
+      done("hello from the backend");
+    }, 15000);
   });
 });
 
 /*
 const wss = new WebSocket.Server({ server });
 const sockets = [];
-
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "Anon";
   console.log("Connected to Browser ✅");
   socket.on("close", onSocketClose);
   socket.on("message", (msg) => {
@@ -35,15 +35,13 @@ wss.on("connection", (socket) => {
     switch (message.type) {
       case "new_message":
         sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}:${message.payload}`)
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
         );
-      //console.log(parsed, message.toString("utf8"));
       case "nickname":
         socket["nickname"] = message.payload;
-      //console.log(message.payload);
     }
   });
-});*/
+}); */
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
